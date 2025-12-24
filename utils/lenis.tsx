fb4d6293634a6f3612bd6083241c6f9bbd69b264
ashbi8137 <ashputhusseri@gmail.com>
2025-12-24 15:40:20 +0530

    Fix: Replace deprecated @studio-freight/react-lenis with lenis

--- utils/lenis.tsx
+++ utils/lenis.tsx
@@ -1,21 +1,18 @@
 "use client";
 
-import { ReactLenis as Lenis } from "@studio-freight/react-lenis";
+import { ReactLenis as Lenis } from "lenis/react";
 
 export function ReactLenis({
   root,
   options,
-  children,
   ...props
 }: {
   root?: boolean;
   options?: any;
   children: React.ReactNode;
-  [key: string]: any;
+  className?: string;
 }) {
   return (
-    <Lenis root={root} options={{ lerp: 0.1, duration: 1.5, smoothWheel: true, ...options }} {...props}>
-      {children}
-    </Lenis>
+    <Lenis root={root} options={{ lerp: 0.1, duration: 1.5, smoothWheel: true, ...options }} {...props} />
   );
 }
